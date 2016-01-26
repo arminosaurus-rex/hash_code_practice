2dfde8dc631130314140529f432494e5811278c8
bob <bob@ksp.sk>
2016-01-26 21:40:52 +0000

    fixing input file format in modulo.cpp

--- slot_to_pools/modulo.cpp
+++ slot_to_pools/modulo.cpp
@@ -14,13 +14,18 @@ struct Server {
 typedef vector<int> Allocation; // server_id -> pool
 
 int main() {
-	int num_servers, num_pools;
-	cin >> num_servers >> num_pools;
+	int num_servers, num_pools, num_rows;
+	cin >> num_servers >> num_pools >> num_rows;
 
 	vector<Server> servers;
 	for (int i = 0; i < num_servers; ++i) {
 		Server cur;
 		cin >> cur.row >> cur.slot >> cur.capacity;
+		if (!((cur.row == -1) == (cur.slot == -1) && (cur.slot == -1) == (cur.capacity == -1))) {
+                    cout << "!!! " << i << endl;
+                    cout << cur.row << " " << cur.slot << " " << cur.capacity << endl;
+		    cout << ((cur.row == -1) == (cur.slot == -1)) << " " << ((cur.slot == -1) == (cur.capacity == -1)) << endl;
+                }
 		assert((cur.row == -1) == (cur.slot == -1) &&
 		       (cur.slot == -1) == (cur.capacity == -1));
 		cur.allocated = (cur.row != -1);
